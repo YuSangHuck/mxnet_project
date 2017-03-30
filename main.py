367a1351455c9f1073bdecc9732eb73b0371f38d
YuSangHuck <g01099387636@gmail.com>
2017-03-30 19:01:17 +0000

    modify code / dataset_dir dependent to Dataset.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import Train
 #out_dataset_dir = 'D:\Github\dataset\img\mydataset\out'
 
 # for linux settings
-in_dataset_dir = '/root/git/dataset'
-out_dataset_dir = in_dataset_dir + '/out_dataset'
+in_dataset_dir = Dataset.in_dataset_dir
+out_dataset_dir = Dataset.out_dataset_dir
 
 if __name__ == '__main__':
-    Dataset.Dataset_create(Dataset.in_dataset_dir, Dataset.out_dataset_dir, 32, 4)
+#    Dataset.Dataset_create(Dataset.in_dataset_dir, Dataset.out_dataset_dir, 32, 4)
+    print(Dataset.Dataset_result(out_dataset_dir))
