77d7a54b01b06e78aa84e27167ae1f508e8494a9
YuSangHuck <g01099387636@gmail.com>
2017-03-30 05:16:02 +0000

    add code / in&out dataset_dir

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import Dataset
 import Train
 
+# for windows settings
+#in_dataset_dir = 'D:\Github\dataset\img\mydataset'
+#out_dataset_dir = 'D:\Github\dataset\img\mydataset\out'
+
+# for linux settings
+in_dataset_dir = '/root/git/dataset'
+out_dataset_dir = in_dataset_dir + '/out_dataset'
+
 if __name__ == '__main__':
     Dataset.Dataset_create(Dataset.in_dataset_dir, Dataset.out_dataset_dir, 32, 4)
