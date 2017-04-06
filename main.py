bebeb71a97c9a917b4b663dd0793310b0eb9d192
YuSangHuck <g01099387636@gmail.com>
2017-04-06 07:49:48 +0000

    change code / easy to look

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ import Train
 # for linux settings
 in_dataset_dir = Dataset.in_dataset_dir
 out_dataset_dir = Dataset.out_dataset_dir
-
+out_model_dir = Train.out_model_dir
 if __name__ == '__main__':
-#    Dataset.Dataset_create(Dataset.in_dataset_dir, Dataset.out_dataset_dir, 32, 4)
+    Dataset.Dataset_create(in_dataset_dir, out_dataset_dir, 32, 4)
     print(Dataset.Dataset_result(out_dataset_dir))
+    Train.Train_create(dataset_dir = out_dataset_dir,
+                       framework = 4,
+                       out_model_dir = out_model_dir,
+                       max_epochs = 4,
+                       mb_size = 128,
+                       network_name = 'lenet',
+                       devs = '1,2')
+    print(Train.Train_result(model_dir=out_model_dir))
+
